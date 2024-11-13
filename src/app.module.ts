89def70cd7eca9bd082a4a8b00abf0717a9439ad
Ben Keys <bkeys818@gmail.com>
2024-11-13 12:12:53 -0500

    🎨 Format code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { CourseModule } from './course/course.module'
+import { StudentModule } from './student/student.module'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'wZYwaR!xfYRzT2n2HC',
+      database: 'college',
+      entities: [],
+      synchronize: true,
+    }),
+    CourseModule,
+    StudentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
